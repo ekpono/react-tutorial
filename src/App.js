094bf643e7e8cf0b06e599f51e5f2f2f83c9bfda
ekpono <ekponoambrose@gmail.com>
2022-01-12 22:33:27 +0100

    i love react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Heaven Man <code>src/App.js</code> and save to reload.
+          I love react JS<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
